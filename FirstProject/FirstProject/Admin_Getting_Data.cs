4b26ed103eb8a9c7a099a48264186e5b3a5e5d5d
Durez <Durez@COM-18>
2018-10-09 15:01:13 +0300

    creation of pocos

---
+++ FirstProject/FirstProject/Admin_Getting_Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToDB;
+using LinqToDB.Common;
+using FirstProject.DbLayer.POCOs;
+
+namespace FirstProject
+{
+    public class Admin_Getting_Data
+    {
+        public static List<Admin_Data> All()
+        {
+            using (var DBO = new Data_Base_One())
+        }
+    }
+}
---
+++ FirstProject/FirstProject/ConectionClass.cs
@@ -0,0 +1,17 @@
+﻿using FirstProject.DbLayer.POCOs;
+using LinqToDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class ConectionClass : LinqToDB.Data.DataConnection
+    {
+        public ConectionClass() : base("Data_Base_One") { }
+        public ITable<FileData> FileData => GetTable<FileData>();
+        public ITable<Admin_Data> Admin_Data => GetTable<Admin_Data>();
+        public ITable<Review_Data> Review_Data => GetTable<Review_Data>();
+    }
+}
